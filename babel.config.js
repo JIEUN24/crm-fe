0b379b5bdb02002e3c672558d417ee673e51f174
jiminChoi <choijming21@naver.com>
2025-07-02 23:06:35 +0900

    chore(config): Locator JS 설치 및 개발 환경 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      ['@babel/preset-react', { runtime: 'automatic' }],
-      '@babel/preset-typescript',
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      '@locator/babel-jsx/dist',
+      {
+        env: 'development',
+      },
     ],
-  };
-  
+  ],
+};
